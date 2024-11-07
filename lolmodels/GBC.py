1c2c14f49f740b10700fdfeadb60806de90c682f
AlixMenard <amenard@ec-m.fr>
2024-11-07 15:45:01 +0100

    Starting to build and optimize models

--- lolmodels/GBC.py
+++ lolmodels/GBC.py
@@ -5,55 +5,72 @@ from torch.ao.nn.quantized import Dropout
 
 class GBC:
 
-    def create(self, n_estimators=10000, max_depth=13, random_state=0, max_features:float|str|int=0.9):
-        return GradientBoostingClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state, max_features=max_features)
+    def create(self, n_estimators=100, max_depth=3, learning_rate=0.1, subsample=1.0):
+        return GradientBoostingClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate, subsample=subsample)
 
-    def train(self, forest, train_set, validation_set):
+    def train(self, gbc, train_set, validation_set):
         X_train, y_train = train_set[:,1:], train_set[:,0]
         X_val, y_val = validation_set[:,1:], validation_set[:,0]
 
-        forest.fit(X_train, y_train)
-        return forest.score(X_val, y_val)
+        gbc.fit(X_train, y_train)
+        return gbc.score(X_val, y_val)
 
     def search_params(self, train_set, validation_set):
         X_train, y_train = train_set[:,1:], train_set[:,0]
         X_val, y_val = validation_set[:,1:], validation_set[:,0]
 
-        #? n_estimators ! 10000
-        """x = list(range(5000, 16000, 1000))
-        accuracy = []
-        for n in x:
-            print(n)
-            forest = self.create(n_estimators=n)
-            forest.fit(X_train, y_train)
-            accuracy.append(forest.score(X_val, y_val))
-        plt.plot(x, accuracy)
-        plt.show()"""
-
-        #? max_depth ! 13
-        """x = list(range(10, 20, 1))
+        #? n_estimators !
+        x = [10,50,100,200,500,1000,5000,10000]
         accuracy = []
         for n in x:
             print(n)
             score = 0
             for _ in range(10):
-                forest = self.create(max_depth=n)
-                forest.fit(X_train, y_train)
-                score += forest.score(X_val, y_val)
+                gbc = self.create(n_estimators=n)
+                gbc.fit(X_train, y_train)
+                score += gbc.score(X_val, y_val)
             accuracy.append(score/10)
         plt.plot(x, accuracy)
-        plt.show()"""
+        plt.show()
 
-        #? max_features ! 0.9
-        """x = [0.85, 0.9, 0.95]
+        #? learning_rate !
+        x = list(i/ 10 for i in range(1, 10, 1))
         accuracy = []
         for n in x:
             print(n)
             score = 0
             for _ in range(10):
-                forest = self.create(max_features=n)
-                forest.fit(X_train, y_train)
-                score += forest.score(X_val, y_val)
+                gbc = self.create(learning_rate=n)
+                gbc.fit(X_train, y_train)
+                score += gbc.score(X_val, y_val)
             accuracy.append(score/10)
         plt.plot(x, accuracy)
-        plt.show()"""
+        plt.show()
+
+        #? max_depth !
+        x = list(range(1, 15, 1))
+        accuracy = []
+        for n in x:
+            print(n)
+            score = 0
+            for _ in range(10):
+                gbc = self.create(max_depth=n)
+                gbc.fit(X_train, y_train)
+                score += gbc.score(X_val, y_val)
+            accuracy.append(score/10)
+        plt.plot(x, accuracy)
+        plt.show()
+
+        #? subsample !
+        x = list(i/ 10 for i in range(1, 10, 1))
+        accuracy = []
+        for n in x:
+            print(n)
+            score = 0
+            for _ in range(10):
+                gbc = self.create(subsample=n)
+                gbc.fit(X_train, y_train)
+                score += gbc.score(X_val, y_val)
+            accuracy.append(score/10)
+        plt.plot(x, accuracy)
+        plt.show()
---
+++ lolmodels/random_forest.py
@@ -0,0 +1,57 @@
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+class RandomForest:
+
+    def create(self, n_estimators=10000, max_depth=13, random_state=0, max_features:float|str|int=0.9):
+        return RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state, max_features=max_features)
+
+    def train(self, forest, train_set, validation_set):
+        X_train, y_train = train_set[:,1:], train_set[:,0]
+        X_val, y_val = validation_set[:,1:], validation_set[:,0]
+
+        forest.fit(X_train, y_train)
+        return forest.score(X_val, y_val)
+
+    def search_params(self, train_set, validation_set):
+        X_train, y_train = train_set[:,1:], train_set[:,0]
+        X_val, y_val = validation_set[:,1:], validation_set[:,0]
+
+        #? n_estimators ! 10000
+        """x = list(range(5000, 16000, 1000))
+        accuracy = []
+        for n in x:
+            print(n)
+            forest = self.create(n_estimators=n)
+            forest.fit(X_train, y_train)
+            accuracy.append(forest.score(X_val, y_val))
+        plt.plot(x, accuracy)
+        plt.show()"""
+
+        #? max_depth ! 13
+        """x = list(range(10, 20, 1))
+        accuracy = []
+        for n in x:
+            print(n)
+            score = 0
+            for _ in range(10):
+                forest = self.create(max_depth=n)
+                forest.fit(X_train, y_train)
+                score += forest.score(X_val, y_val)
+            accuracy.append(score/10)
+        plt.plot(x, accuracy)
+        plt.show()"""
+
+        #? max_features ! 0.9
+        """x = [0.85, 0.9, 0.95]
+        accuracy = []
+        for n in x:
+            print(n)
+            score = 0
+            for _ in range(10):
+                forest = self.create(max_features=n)
+                forest.fit(X_train, y_train)
+                score += forest.score(X_val, y_val)
+            accuracy.append(score/10)
+        plt.plot(x, accuracy)
+        plt.show()"""
