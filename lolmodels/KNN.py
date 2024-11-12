e65726df4e0e011d56a4d05f0000f286861f2099
AlixMenard <amenard@ec-m.fr>
2024-11-12 13:50:28 +0100

    KNN model

---
+++ lolmodels/KNN.py
@@ -0,0 +1,79 @@
+from sklearn.neighbors import KNeighborsClassifier as NN
+import numpy as np
+import matplotlib.pyplot as plt
+
+class KNN:
+
+    def create(self, n_neighbours=5, weights='uniform', algorithm='ball_tree', metric='minkowski', p=2):
+        return NN(n_neighbors=n_neighbours, weights=weights, algorithm=algorithm, metric=metric, p=p)
+        #weights : ‘uniform’, ‘distance’
+        #algo : ‘ball_tree’, ‘kd_tree’, ‘brute’
+
+    def train(self, knn, train_set, validation_set):
+        #np.random.shuffle(train_set)
+        #np.random.shuffle(validation_set)
+
+        X_train, y_train = train_set[:,1:], train_set[:,0]
+        X_val, y_val = validation_set[:,1:], validation_set[:,0]
+
+        knn.fit(X_train, y_train)
+        return knn.score(X_val, y_val)
+
+    def search_params(self, train_set, validation_set):
+        X_train, y_train = train_set[:,1:], train_set[:,0]
+        X_val, y_val = validation_set[:,1:], validation_set[:,0]
+
+        #? n_neighbours ! 39
+        x = list(range(37,42,1))
+        accuracy = []
+        for n in x:
+            print(n)
+            knn = self.create(n_neighbours=n)
+            knn.fit(X_train, y_train)
+            accuracy.append(knn.score(X_val, y_val))
+        plt.plot(x, accuracy)
+        plt.show()
+
+        #? weights ! idem
+        """w = ['uniform', 'distance']
+        x = [0,1]
+        accuracy = []
+        for n in x:
+            print(n)
+            score = 0
+            for _ in range(10):
+                knn = self.create(weights=w[n])
+                knn.fit(X_train, y_train)
+                score += knn.score(X_val, y_val)
+            accuracy.append(score/10)
+        plt.plot(x, accuracy)
+        plt.show()"""
+
+        #? algorithm ! idem
+        """a = ['ball_tree', 'kd_tree', 'brute']
+        x = [0,1,2]
+        accuracy = []
+        for n in x:
+            print(n)
+            score = 0
+            for _ in range(10):
+                knn = self.create(algorithm=a[n])
+                knn.fit(X_train, y_train)
+                score += knn.score(X_val, y_val)
+            accuracy.append(score/10)
+        plt.plot(x, accuracy)
+        plt.show()"""
+
+        #? p !
+        x = list(range(16,20,1))
+        accuracy = []
+        for n in x:
+            print(n)
+            score = 0
+            for _ in range(10):
+                knn = self.create(p=n/10)
+                knn.fit(X_train, y_train)
+                score += knn.score(X_val, y_val)
+            accuracy.append(score/10)
+        plt.plot(x, accuracy)
+        plt.show()
